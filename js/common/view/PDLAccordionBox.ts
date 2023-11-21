9772bdabdb19d48215fd0c9163629d97ea7165cb
phet-dev <phetadmin@gmail.com>
2023-11-21 03:18:16 -0700

    update copyright dates from daily grunt work

--- js/common/view/PDLAccordionBox.ts
+++ js/common/view/PDLAccordionBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2023, University of Colorado Boulder
 
 /**
  * The PDLAccordionBox is an accordion UI component for the Projectile Data Lab simulation.
