43b439220ebf9d0ed961fff89e52389c0ef7b95e
matthew-blackman <mabl4992@colorado.edu>
2023-11-21 11:01:41 -0500

    Create HeatMapToolNode - see https://github.com/phetsims/projectile-data-lab/issues/7

---
+++ js/common-vsm/view/HeatMapToolNode.ts
@@ -0,0 +1,28 @@
+// Copyright 2023, University of Colorado Boulder
+
+import optionize from '../../../../phet-core/js/optionize.js';
+import { Node, NodeOptions } from '../../../../scenery/js/imports.js';
+import projectileDataLab from '../../projectileDataLab.js';
+import Property from '../../../../axon/js/Property.js';
+
+/**
+ * The HeatMapToolNode is a base class for tool nodes that show a heat map representation of data. It consists of an
+ * array of graphical elements that update opacity as the data changes, as well as background and foreground graphics.
+ *
+ * @author Matthew Blackman (PhET Interactive Simulations)
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+type SelfOptions = {
+  sourceDataProperty: Property<number>;
+};
+export type HeatMapToolNodeOptions = SelfOptions & NodeOptions;
+
+export default class HeatMapToolNode extends Node {
+  public constructor( providedOptions: HeatMapToolNodeOptions ) {
+    const options = optionize<HeatMapToolNodeOptions, SelfOptions, NodeOptions>()( {}, providedOptions );
+    super( options );
+  }
+}
+
+projectileDataLab.register( 'HeatMapToolNode', HeatMapToolNode );
