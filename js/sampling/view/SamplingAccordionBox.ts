5985b6af81cad8548866793702b595747515c6a1
phet-dev <phetadmin@gmail.com>
2023-11-22 03:18:14 -0700

    update copyright dates from daily grunt work

--- js/sampling/view/SamplingAccordionBox.ts
+++ js/sampling/view/SamplingAccordionBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2023, University of Colorado Boulder
 
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 import { Node, Text } from '../../../../scenery/js/imports.js';
