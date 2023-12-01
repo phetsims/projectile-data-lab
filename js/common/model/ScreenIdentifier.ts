535aa9cd536874fd1c726e9700952fc1c20cfb21
Sam Reid <reids@colorado.edu>
2023-12-01 12:44:42 -0700

    Update screen identifiers to include "Screen" suffix, see https://github.com/phetsims/projectile-data-lab/issues/7

--- js/common/model/ScreenIdentifier.ts
+++ js/common/model/ScreenIdentifier.ts
@@ -1,4 +1,4 @@
 // Copyright 2023, University of Colorado Boulder
-export const ScreenIdentifierValues = [ 'variability', 'sources', 'measures', 'sampling' ] as const;
+export const ScreenIdentifierValues = [ 'variabilityScreen', 'sourcesScreen', 'measuresScreen', 'samplingScreen' ] as const;
 
 export type ScreenIdentifier = typeof ScreenIdentifierValues[number];
