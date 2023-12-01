a5287348036006c905728c605048c27fb2d1c3a7
matthew-blackman <mabl4992@colorado.edu>
2023-11-30 20:59:00 -0500

    Remove review comment as complete - see https://github.com/phetsims/projectile-data-lab/issues/7

--- js/common/model/ProjectileType.ts
+++ js/common/model/ProjectileType.ts
@@ -1,6 +1,5 @@
 // Copyright 2023, University of Colorado Boulder
 
-// REVIEW: Do we want to use this pattern for our enums, or EnumerationValue? I didn't know we would have to put 'as const' at other usage sites too.
 export const ProjectileTypeValues = [ 'CANNONBALL', 'PUMPKIN', 'TOY_PIANO' ] as const;
 
 export type ProjectileType = typeof ProjectileTypeValues[number];
