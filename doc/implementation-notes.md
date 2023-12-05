0385bfa8849b0596e721b3a627f6e9e328e21eb1
Sam Reid <reids@colorado.edu>
2023-12-04 17:56:17 -0700

    Address TODO and add documentation, see https://github.com/phetsims/projectile-data-lab/issues/7

--- doc/implementation-notes.md
+++ doc/implementation-notes.md
@@ -1,1 +1,12 @@
 TODO implementation overview
+
+### Scenes
+
+This simulation has a notion of scenes where each scene contains its own state data. The scenes in this simulation are
+called `Field`s. The models that contain `Field[]` also provide `DynamicProperty` to present the value of the selected
+Field.
+
+### Enumeration patterns
+
+For simulation-specific enumerations, we use the string literal union pattern. See LauncherConfiguration.ts for an
+example.
--- js/common/model/Field.ts
+++ js/common/model/Field.ts
@@ -82,9 +82,7 @@ export default abstract class Field extends PhetioObject {
     this.projectilesClearedEmitter = new Emitter();
 
     this.launcherConfigurationProperty = new Property<LauncherConfiguration>( 'ANGLE_45', {
-
-      // TODO: On the sampling screen, only allow valid value of 30.  So make the LauncherConfigurationValues an option passed in, see https://github.com/phetsims/projectile-data-lab/issues/7
-      validValues: LauncherConfigurationValues,
+      validValues: [ 'ANGLE_45' ],
 
       // TODO: Do not instrument on the sampling screen, see https://github.com/phetsims/projectile-data-lab/issues/7
       tandem: providedOptions.tandem.createTandem( 'launcherConfigurationProperty' ),
