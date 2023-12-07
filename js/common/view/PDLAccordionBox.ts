fe6b3fd0b866ee66e350c60fec8c35bb61b30a56
Sam Reid <reids@colorado.edu>
2023-12-07 09:26:24 -0700

    Add total bins combo box, see https://github.com/phetsims/projectile-data-lab/issues/7

--- js/common/view/PDLAccordionBox.ts
+++ js/common/view/PDLAccordionBox.ts
@@ -15,13 +15,13 @@ import WithRequired from '../../../../phet-core/js/types/WithRequired.js';
 import projectileDataLab from '../../projectileDataLab.js';
 import Property from '../../../../axon/js/Property.js';
 import ComboBox from '../../../../sun/js/ComboBox.js';
-import PDLConstants from '../PDLConstants.js';
 import PDLColors from '../PDLColors.js';
 import PDLText from './PDLText.js';
 import ToggleNode from '../../../../sun/js/ToggleNode.js';
 import { BIN_STRATEGY_PROPERTY } from '../PDLQueryParameters.js';
 import PatternStringProperty from '../../../../axon/js/PatternStringProperty.js';
 import ProjectileDataLabStrings from '../../ProjectileDataLabStrings.js';
+import Utils from '../../../../dot/js/Utils.js';
 
 type SelfOptions = {
   binWidthProperty: Property<number>;
@@ -36,25 +36,44 @@ export default class PDLAccordionBox extends AccordionBox {
 
     const margin = 10;
 
-    const comboBoxItems = [];
-    const validBinWidths = providedOptions.binWidthProperty.validValues ?? [];
-    for ( let i = 0; i < validBinWidths.length; i++ ) {
-      const binWidth = validBinWidths[ i ];
-      comboBoxItems.push( {
-        value: binWidth,
-        createNode: () => new PDLText( new PatternStringProperty( ProjectileDataLabStrings.binWidthMPatternStringProperty, {
-          binWidth: binWidth
-        } ) ),
-        tandemName: `binWidth${binWidth}Item`
-      } );
+    class BinWidthComboBox extends ComboBox<number> {
+      public constructor() {
+        const validBinWidths = providedOptions.binWidthProperty.validValues ?? [];
+        const comboBoxItems = validBinWidths.map( binWidth => {
+          return {
+            value: binWidth,
+            createNode: () => new PDLText( new PatternStringProperty( ProjectileDataLabStrings.binWidthMPatternStringProperty, {
+              binWidth: binWidth
+            } ) ),
+            tandemName: `binWidth${binWidth}Item`
+          };
+        } );
+        super( providedOptions.binWidthProperty, comboBoxItems, comboBoxParent, {
+          listPosition: 'below',
+          tandem: providedOptions.tandem.createTandem( 'binWidthComboBox' )
+        } );
+      }
     }
 
-    const binWidthComboBox = new ComboBox( providedOptions.binWidthProperty, comboBoxItems, comboBoxParent, {
-      listPosition: 'below',
-      tandem: providedOptions.tandem.createTandem( 'binWidthComboBox' )
-    } );
+    class TotalBinsComboBox extends ComboBox<number> {
+      public constructor() {
+        const validTotalBins = [ 1, 2, 5, 10, 20, 25, 50, 100 ];
+        const comboBoxItems = validTotalBins.map( totalBins => {
+          return {
+            value: totalBins,
+            createNode: () => new PDLText( Utils.toFixed( totalBins, 0 ) ),
+            tandemName: `totalBins${totalBins}Item`
+          };
+        } );
+        super( providedOptions.binWidthProperty, comboBoxItems, comboBoxParent, {
+          listPosition: 'below',
+          tandem: providedOptions.tandem.createTandem( 'totalBinsComboBox' )
+        } );
+      }
+    }
 
     const labelAndComboBoxContainer = new HBox( {
+      align: 'center',
       children: [
         new ToggleNode( BIN_STRATEGY_PROPERTY, [ {
           value: 'binWidth',
@@ -65,13 +84,23 @@ export default class PDLAccordionBox extends AccordionBox {
         } ], {
           alignChildren: ToggleNode.RIGHT,
           layoutOptions: {
-            topMargin: PDLConstants.PRIMARY_FONT.getNumericSize() / 2,
+            rightMargin: margin
+          },
+          tandem: providedOptions.tandem.createTandem( 'binWidthLabel' )
+        } ),
+        new ToggleNode( BIN_STRATEGY_PROPERTY, [ {
+          value: 'binWidth',
+          createNode: () => new BinWidthComboBox()
+        }, {
+          value: 'totalBins',
+          createNode: () => new TotalBinsComboBox()
+        } ], {
+          alignChildren: ToggleNode.LEFT,
+          layoutOptions: {
             rightMargin: margin
           }
-        } ),
-        binWidthComboBox
-      ],
-      align: 'top'
+        } )
+      ]
     } );
 
     const contentContainer = new VBox( {
