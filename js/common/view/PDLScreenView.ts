eba979a35ea0e0ad4d691c30f91e7061c3bfb8d3
Sam Reid <reids@colorado.edu>
2023-12-09 20:25:48 -0700

    Improve dynamic layout, see https://github.com/phetsims/projectile-data-lab/issues/7

--- js/common/view/PDLScreenView.ts
+++ js/common/view/PDLScreenView.ts
@@ -97,13 +97,13 @@ export default abstract class PDLScreenView<T extends Field> extends ScreenView 
     } );
 
     this.noAirResistanceText = new PDLText( ProjectileDataLabStrings.noAirResistanceStringProperty, {
-      layoutOptions: {
-        align: 'right'
-      },
-      right: this.layoutBounds.maxX - PDLConstants.SCREEN_VIEW_X_MARGIN,
       bottom: PDLConstants.FIELD_SIGN_CENTER_Y - PDLConstants.FIELD_SIGN_AIR_RESISTANCE_TEXT_SEPARATION,
       font: PDLConstants.NO_AIR_RESISTANCE_FONT,
-      maxWidth: 200
+      maxWidth: 160
+    } );
+
+    ProjectileDataLabStrings.noAirResistanceStringProperty.link( () => {
+      this.noAirResistanceText.right = this.layoutBounds.right - PDLConstants.SCREEN_VIEW_X_MARGIN;
     } );
 
     this.resetAllButton = new ResetAllButton( {
