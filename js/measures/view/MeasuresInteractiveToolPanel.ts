8a4939894bb060c22e6e6437092ee9c666ceb7e1
Sam Reid <reids@colorado.edu>
2023-12-13 06:58:26 -0700

    Add interval tool icon, see https://github.com/phetsims/projectile-data-lab/issues/7

--- js/measures/view/MeasuresInteractiveToolPanel.ts
+++ js/measures/view/MeasuresInteractiveToolPanel.ts
@@ -3,10 +3,15 @@
 import projectileDataLab from '../../projectileDataLab.js';
 import { PDLPanelOptions } from '../../common/view/PDLPanel.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
-import { Rectangle } from '../../../../scenery/js/imports.js';
+import { Node } from '../../../../scenery/js/imports.js';
 import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import ProjectileDataLabStrings from '../../ProjectileDataLabStrings.js';
 import InteractiveToolPanel, { InteractiveToolPanelOptions } from '../../common-vsm/view/InteractiveToolPanel.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
+import IntervalToolNode from './IntervalToolNode.js';
+import IntervalTool from '../model/IntervalTool.js';
+import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
 
 type SelfOptions = EmptySelfOptions;
 type MeasuresInteractiveToolPanelOptions = SelfOptions & InteractiveToolPanelOptions;
@@ -18,11 +23,31 @@ export default class MeasuresInteractiveToolPanel extends InteractiveToolPanel {
     isIntervalToolVisibleProperty: BooleanProperty,
     providedOptions: PDLPanelOptions ) {
 
+    class IntervalToolIcon extends Node {
+      public constructor() {
+
+        // Create a ModelViewTransform2 that defines how to scale the relative sizes of the interval tool components.
+        // A smaller scale emphasizes the text elements, a larger scale shrinks the text.
+        const transform = ModelViewTransform2.createSinglePointScaleInvertedYMapping( new Vector2( 0, 0 ), new Vector2( 0, 0 ), 5 );
+        const intervalTool = new IntervalTool( {
+          tandem: Tandem.OPT_OUT
+        } );
+        const intervalToolNode = new IntervalToolNode( intervalTool, transform, new BooleanProperty( true ), {
+          tandem: Tandem.OPT_OUT
+        } ).rasterized( { resolution: 2 } );
+        super( {
+          children: [ intervalToolNode ],
+          pickable: false,
+          maxWidth: 25
+        } );
+      }
+    }
+
     const options = optionize<PDLPanelOptions, SelfOptions, MeasuresInteractiveToolPanelOptions>()( {
       additionalVerticalCheckboxGroupItems: [
         {
           property: isIntervalToolVisibleProperty,
-          createNode: () => InteractiveToolPanel.createCheckboxRow( ProjectileDataLabStrings.intervalToolStringProperty, new Rectangle( 0, 0, 12, 12, { fill: 'red' } ) ),
+          createNode: () => InteractiveToolPanel.createCheckboxRow( ProjectileDataLabStrings.intervalToolStringProperty, new IntervalToolIcon() ),
           tandemName: 'intervalToolCheckbox'
         }
       ]
