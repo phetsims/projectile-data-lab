706d521243168969f538c54a7aeb5adbbeb316a8
Sam Reid <reids@colorado.edu>
2023-12-13 22:45:27 -0700

    Remove unnecessary TODO, see https://github.com/phetsims/projectile-data-lab/issues/7

--- js/common/view/PDLCanvasNode.ts
+++ js/common/view/PDLCanvasNode.ts
@@ -108,8 +108,6 @@ export default abstract class PDLCanvasNode<T extends Field> extends CanvasNode 
     };
 
     for ( let t = 0; t < projectile.timeAirborne; t += 0.02 ) {
-
-      // TODO: Should we use quadraticCurveTo? See https://github.com/phetsims/projectile-data-lab/issues/7
       drawLineToProjectileAtTime( t );
     }
 
