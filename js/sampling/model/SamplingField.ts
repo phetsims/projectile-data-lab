2d0b22842ea902f7449c3a88481d28a4ee021e24
matthew-blackman <mabl4992@colorado.edu>
2024-01-04 13:14:14 -0500

    Update continuous sample timing - see https://github.com/phetsims/projectile-data-lab/issues/7

--- js/sampling/model/SamplingField.ts
+++ js/sampling/model/SamplingField.ts
@@ -29,7 +29,7 @@ export type SamplingFieldOptions = SelfOptions & FieldOptions;
 const SHOWING_CLEAR_PRESAMPLE_TIME = 0;
 const SHOWING_SAMPLE_TIME = 0;
 const SHOWING_SINGLE_SAMPLE_TIME = 0.5;
-const SHOWING_SAMPLE_AND_MEAN_TIME = 0.2;
+const SHOWING_SAMPLE_AND_MEAN_TIME = 0.15;
 
 // TODO: Clean up after we find out if we are keeping https://github.com/phetsims/projectile-data-lab/commit/548b36f3b75921d7c50105a7d49daab8c56b39a0, see https://github.com/phetsims/projectile-data-lab/issues/17
 // In single mode, we transition through these phases:
