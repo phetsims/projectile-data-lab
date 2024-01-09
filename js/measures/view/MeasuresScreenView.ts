baaf1286c04827dc1486186d99290d71bc0c004b
Sam Reid <reids@colorado.edu>
2024-01-09 07:33:33 -0700

    Move TODO, see https://github.com/phetsims/projectile-data-lab/issues/60

--- js/measures/view/MeasuresScreenView.ts
+++ js/measures/view/MeasuresScreenView.ts
@@ -96,7 +96,7 @@ export default class MeasuresScreenView extends VSMScreenView<MeasuresField> {
     this.toolsLayer.addChild( intervalToolNode );
     this.toolsLayer.addChild( dataMeasuresFieldOverlay );
 
-    // Keyboard order. TODO: How to eliminate duplication with the parent class? See https://github.com/phetsims/projectile-data-lab/issues/44
+    // Keyboard order. TODO: How to eliminate duplication with the parent class? See https://github.com/phetsims/projectile-data-lab/issues/60
     this.pdomControlAreaNode.pdomOrder = [
       this.launchPanel,
       this.launchButton,
