a232a8be57cc25be7c0d9111f5eeaca06d7360a6
phet-dev <phetadmin@gmail.com>
2024-01-10 03:16:27 -0700

    update copyright dates from daily grunt work

--- js/common/view/SelectorNode.ts
+++ js/common/view/SelectorNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2024, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 import { Color, Node, NodeOptions, Path, SceneryConstants } from '../../../../scenery/js/imports.js';
 import AccessibleNumberSpinner, { AccessibleNumberSpinnerOptions } from '../../../../sun/js/accessibility/AccessibleNumberSpinner.js';
