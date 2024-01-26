6307cb3014deff0fbb89b4ae8030debc436a59eb
phet-dev <phetadmin@gmail.com>
2024-01-26 03:17:03 -0700

    update copyright dates from daily grunt work

--- js/common-sm/model/SMField.ts
+++ js/common-sm/model/SMField.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * The SMField adds functionality common between the Fields in the Sources and Measures screens.
