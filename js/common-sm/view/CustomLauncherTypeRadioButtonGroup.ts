3cf396980e1ac4dc3815c191df79ff91a8465760
matthew-blackman <mabl4992@colorado.edu>
2024-02-22 18:12:33 -0500

    Remove unused launch mechanism sounds - see https://github.com/phetsims/projectile-data-lab/issues/126

--- js/common-sm/view/CustomLauncherTypeRadioButtonGroup.ts
+++ js/common-sm/view/CustomLauncherTypeRadioButtonGroup.ts
@@ -19,27 +19,19 @@ import explosion_png from '../../../images/explosion_png.js';
 import LauncherMechanism, { EXPLOSION, PRESSURE, SPRING } from '../../common-vsm/model/LauncherMechanism.js';
 import SoundClip from '../../../../tambo/js/sound-generators/SoundClip.js';
 import soundManager from '../../../../tambo/js/soundManager.js';
-import pdlCustomLauncherButtonSpringv2_mp3 from '../../../sounds/pdlCustomLauncherButtonSpringv2_mp3.js';
-import launcherMechanismButtonPressure2_mp3 from '../../../sounds/launcherMechanismButtonPressure2_mp3.js';
+import launcherMechanismButtonSpring_mp3 from '../../../sounds/launcherMechanismButtonSpring_mp3.js';
 import launcherMechanismButtonPressure1_mp3 from '../../../sounds/launcherMechanismButtonPressure1_mp3.js';
-import launcherMechanismButtonPressure3_mp3 from '../../../sounds/launcherMechanismButtonPressure3_mp3.js';
 import launcherMechanismButtonExplosion_mp3 from '../../../sounds/launcherMechanismButtonExplosion_mp3.js';
 import PDLRectangularRadioButtonGroup from '../../common/view/PDLRectangularRadioButtonGroup.js';
 
 type SelfOptions = EmptySelfOptions;
 type CustomLauncherTypeRadioButtonGroupOptions = SelfOptions & WithRequired<RectangularRadioButtonGroupOptions, 'tandem'>;
 
-const springSound = new SoundClip( pdlCustomLauncherButtonSpringv2_mp3 );
+const springSound = new SoundClip( launcherMechanismButtonSpring_mp3 );
 soundManager.addSoundGenerator( springSound, { categoryName: 'user-interface' } );
 
-const pressureSound1 = new SoundClip( launcherMechanismButtonPressure1_mp3 );
-soundManager.addSoundGenerator( pressureSound1, { categoryName: 'user-interface' } );
-
-const pressureSound2 = new SoundClip( launcherMechanismButtonPressure2_mp3 );
-soundManager.addSoundGenerator( pressureSound2, { categoryName: 'user-interface' } );
-
-const pressureSound3 = new SoundClip( launcherMechanismButtonPressure3_mp3 );
-soundManager.addSoundGenerator( pressureSound3, { categoryName: 'user-interface' } );
+const pressureSound = new SoundClip( launcherMechanismButtonPressure1_mp3 );
+soundManager.addSoundGenerator( pressureSound, { categoryName: 'user-interface' } );
 
 const explosionSound = new SoundClip( launcherMechanismButtonExplosion_mp3 );
 soundManager.addSoundGenerator( explosionSound, { categoryName: 'user-interface' } );
@@ -60,7 +52,7 @@ export default class CustomLauncherTypeRadioButtonGroup extends PDLRectangularRa
         tandemName: 'pressureRadioButton',
         createNode: () => new Image( pressureWithNeedle_png, { scale: 0.18 } ),
         options: {
-          soundPlayer: pressureSound1
+          soundPlayer: pressureSound
         }
       }, {
         value: EXPLOSION,
