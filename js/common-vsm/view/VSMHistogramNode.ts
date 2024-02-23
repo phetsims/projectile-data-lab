01e80bb8ad563a27c5bb05f5f9343ae606fa69d5
phet-dev <phetadmin@gmail.com>
2024-02-23 03:21:10 -0700

    update copyright dates from daily grunt work

--- js/common-vsm/view/VSMHistogramNode.ts
+++ js/common-vsm/view/VSMHistogramNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 
 import { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
