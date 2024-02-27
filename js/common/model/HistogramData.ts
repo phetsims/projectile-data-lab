3bde0d745fe62793ca3f32fd5437b00d9f0d7745
phet-dev <phetadmin@gmail.com>
2024-02-27 03:21:30 -0700

    update copyright dates from daily grunt work

--- js/common/model/HistogramData.ts
+++ js/common/model/HistogramData.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * Represents values that can be depicted in the histogram. For the first 3 screens, the data for the histogram is one
