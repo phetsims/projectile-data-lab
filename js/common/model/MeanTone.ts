d03005947d533c0aec51978f51649b443398b477
matthew-blackman <mabl4992@colorado.edu>
2024-02-28 10:57:09 -0500

    Update mean tone and pitch mapping - see https://github.com/phetsims/projectile-data-lab/issues/161

--- js/common/model/MeanTone.ts
+++ js/common/model/MeanTone.ts
@@ -8,39 +8,17 @@
  */
 
 import projectileDataLab from '../../projectileDataLab.js';
-import phetAudioContext from '../../../../tambo/js/phetAudioContext.js';
-import { playbackRateForPosition } from './ProjectileSound.js';
 import SoundClip from '../../../../tambo/js/sound-generators/SoundClip.js';
-import numberTone_mp3 from '../../../sounds/numberTone_mp3.js';
+import meanTone_wav from '../../../sounds/meanTone_wav.js';
 import soundManager from '../../../../tambo/js/soundManager.js';
+import Utils from '../../../../dot/js/Utils.js';
 
-// This is the dominant frequency of numberTone_mp3. If the audio file is changed, this will need to be updated.
-const E3 = 164.81; // Hz
-
-const INITIAL_OUTPUT_LEVEL = 0.2;
-
-// Filter to make the mean sound different from the main sound and from the median sound
-const meanFilter = new BiquadFilterNode( phetAudioContext, {
-  type: 'bandpass',
-  Q: 2
-} );
-
-const meanSoundClip = new SoundClip( numberTone_mp3, {
-  initialOutputLevel: INITIAL_OUTPUT_LEVEL,
-  additionalAudioNodes: [ meanFilter ]
-} );
-
+const meanSoundClip = new SoundClip( meanTone_wav, { initialOutputLevel: 0.2 } );
 soundManager.addSoundGenerator( meanSoundClip );
 
 export class MeanTone {
   public static playMean( value: number ): void {
-    const playbackRate = playbackRateForPosition( value );
-
-    // set the frequency of the band pass filter to be equal to the frequency of the adjusted sound
-    const frequency = E3 * playbackRate;
-    meanFilter.frequency.setTargetAtTime( frequency, phetAudioContext.currentTime, 0 );
-
-    meanSoundClip.setPlaybackRate( playbackRate );
+    meanSoundClip.setPlaybackRate( Utils.linear( 0, 100, 1, 2, value ) );
     meanSoundClip.play();
   }
 }
