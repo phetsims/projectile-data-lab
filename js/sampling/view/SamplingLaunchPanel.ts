c6660c2a11509721b2066f7f381b792b5a26432a
matthew-blackman <mabl4992@colorado.edu>
2024-03-05 15:44:40 -0500

    Update optionize for SamplingLaunchPanel - see https://github.com/phetsims/projectile-data-lab/issues/219

--- js/sampling/view/SamplingLaunchPanel.ts
+++ js/sampling/view/SamplingLaunchPanel.ts
@@ -7,20 +7,18 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import { PDLPanelOptions } from '../../common/view/PDLPanel.js';
 import projectileDataLab from '../../projectileDataLab.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 import Property from '../../../../axon/js/Property.js';
 import { Node } from '../../../../scenery/js/imports.js';
 import SampleSizeSection from './SampleSizeSection.js';
-import { PDLLaunchPanel } from '../../common/view/PDLLaunchPanel.js';
+import { PDLLaunchPanel, PDLLaunchPanelOptions } from '../../common/view/PDLLaunchPanel.js';
 import PhetioProperty from '../../../../axon/js/PhetioProperty.js';
 import MysteryLauncherSection from '../../common/view/MysteryLauncherSection.js';
-import WithRequired from '../../../../phet-core/js/types/WithRequired.js';
 import Launcher from '../../common/model/Launcher.js';
 
 type SelfOptions = EmptySelfOptions;
-type SamplingLaunchPanelOptions = SelfOptions & WithRequired<PDLPanelOptions, 'tandem'>;
+type SamplingLaunchPanelOptions = SelfOptions & PDLLaunchPanelOptions;
 
 export default class SamplingLaunchPanel extends PDLLaunchPanel {
 
@@ -35,7 +33,7 @@ export default class SamplingLaunchPanel extends PDLLaunchPanel {
 
     const content: Node[] = [ launcherSection, sampleSizeSection ];
 
-    const options = optionize<SamplingLaunchPanelOptions, SelfOptions, PDLPanelOptions>()( {
+    const options = optionize<SamplingLaunchPanelOptions, SelfOptions, PDLLaunchPanelOptions>()( {
       yMargin: 8
     }, providedOptions );
 
