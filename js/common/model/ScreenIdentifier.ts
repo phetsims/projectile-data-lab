84f78671f3fd2a0688db10fe6b5392f0e88e6e37
matthew-blackman <mabl4992@colorado.edu>
2024-04-24 13:40:54 -0400

    Remove 'screen' from screen identifier values - see https://github.com/phetsims/projectile-data-lab/issues/314

--- js/common/model/ScreenIdentifier.ts
+++ js/common/model/ScreenIdentifier.ts
@@ -5,6 +5,6 @@
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
-export const ScreenIdentifierValues = [ 'variabilityScreen', 'sourcesScreen', 'measuresScreen', 'samplingScreen' ] as const;
+export const ScreenIdentifierValues = [ 'variability', 'sources', 'measures', 'sampling' ] as const;
 
 export type ScreenIdentifier = typeof ScreenIdentifierValues[number];
