64fefdcd089d8cb978b5359ec60fa6d86a1d91db
phet-dev <phetadmin@gmail.com>
2025-01-16 03:32:24 -0700

    update copyright dates from daily grunt work

--- js/sampling/view/SamplingScreenView.ts
+++ js/sampling/view/SamplingScreenView.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2023-2025, University of Colorado Boulder
 
 /**
  * The Sampling Screen View shows the view for the Sampling screen in the Projectile Data Lab.
